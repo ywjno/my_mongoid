33016bf9e020fc27eee8f194621a92f475ac3004
ywjno <ywjno.dev@gmail.com>
2014-02-25 23:31:57 +0800

    `Model attributes and new` mission completed

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -10,6 +10,27 @@ module MyMongoid
       MyMongoid.models << base
     end
 
+    attr_reader :attributes, :new_record
+
+    def initialize(options={})
+      raise ArgumentError unless options.is_a? Hash
+      @attributes = options
+      @new_record = true
+      self
+    end
+
+    def read_attribute(key)
+      @attributes[key]
+    end
+
+    def write_attribute(key, value)
+      @attributes[key] = value
+    end
+
+    def new_record?
+      @new_record
+    end
+
     module ClassMethods
       def is_mongoid_model?
         true
