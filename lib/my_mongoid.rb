015542e655be822bcbb6adc08f42e3d107caf29c
ywjno <ywjno.dev@gmail.com>
2014-02-26 09:51:14 +0800

    `Declare fields` mission completed

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -4,9 +4,19 @@ module MyMongoid
   def self.models
     @models ||= []
   end
+
+  class Field
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name.to_s
+    end
+  end
+
   module Document
     def self.included(base)
       base.extend(ClassMethods)
+      base.field('_id')
       MyMongoid.models << base
     end
 
@@ -19,12 +29,12 @@ module MyMongoid
       self
     end
 
-    def read_attribute(key)
-      @attributes[key]
+    def read_attribute(attr)
+      @attributes[attr.to_s]
     end
 
-    def write_attribute(key, value)
-      @attributes[key] = value
+    def write_attribute(attr, value)
+      @attributes[attr.to_s] = value
     end
 
     def new_record?
@@ -35,6 +45,28 @@ module MyMongoid
       def is_mongoid_model?
         true
       end
+
+      def field(field_name)
+        field_name = field_name.to_s
+        raise DuplicateFieldError if fields.key?(field_name)
+        fields[field_name] = MyMongoid::Field.new(field_name)
+        class_eval %Q{
+          def #{field_name}
+            self.read_attribute('#{field_name}')
+          end
+
+          def #{field_name}=(value)
+            self.write_attribute('#{field_name}', value)
+          end
+        }
+      end
+
+      def fields
+        @fields ||= {}
+      end
     end
+  end
+
+  class DuplicateFieldError < RuntimeError
   end
 end
